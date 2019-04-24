6c97dcdae9e212f6056a27270933688c1c87d4c2
alperolmezz <49038591+alperolmezz@users.noreply.github.com>
2019-04-24 14:25:43 +0300

    Add files via upload

---
+++ uygulama04.py
@@ -0,0 +1,20 @@
+#1.soru
+
+metin ="açık bilim,araştırma çıktılarına ve süreçlerine herkesin serbestçe erişmesini,bunların ortak kullanımını, dağıtımını ve üretimini kolaylaştıran bilim uygulamasıdır."
+print(len(metin))
+
+#2.soru
+from random import choice
+
+liste = choice (["açık bilim","açık erişim","açık lisans","açık eğitim","açık veri","açık kültür"])
+
+
+#3.soru
+
+
+sözlük = input("bir kelime giriniz")
+
+if sözlük == "elma":
+    print("ağaçta yetişen bir tür meyve")
+elif sözlük == "salatalık":
+    print("fidan üzerinde büyüyen bir tür sebze")
